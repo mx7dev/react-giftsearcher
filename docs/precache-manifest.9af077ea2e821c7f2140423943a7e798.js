d1e65e74b865259dea412642ef0ef97180e6b7d6
Maximo Carrion <maximo.carrion.dev@gmail.com>
2020-09-26 13:28:43 -0500

    docs agregado

---
+++ docs/precache-manifest.9af077ea2e821c7f2140423943a7e798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c81ab37385f4f7555c4309fcad27385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e6133aa0f3d95ab332",
+    "url": "/static/css/main.1403b1eb.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e6133aa0f3d95ab332",
+    "url": "/static/js/main.d3b1fa35.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
